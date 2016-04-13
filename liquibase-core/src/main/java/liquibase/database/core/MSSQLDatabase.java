f1b178820fb32aaa1da3857ad661f2307c39366f
Robert Rettig <rrettig@synapplix.de>
2016-04-13 23:48:12 +0200

    fix MSSQL keywords

--- liquibase-core/src/main/java/liquibase/database/core/MSSQLDatabase.java
+++ liquibase-core/src/main/java/liquibase/database/core/MSSQLDatabase.java
@@ -16,6 +16,7 @@ import liquibase.exception.UnexpectedLiquibaseException;
 import liquibase.executor.ExecutorService;
 import liquibase.statement.core.GetViewDefinitionStatement;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -29,6 +30,7 @@ import liquibase.logging.LogFactory;
 public class MSSQLDatabase extends AbstractJdbcDatabase {
     public static final String PRODUCT_NAME = "Microsoft SQL Server";
     protected Set<String> systemTablesAndViews = new HashSet<String>();
+    private static Set<String> reservedWords = new HashSet();
 
     private static Pattern INITIAL_COMMENT_PATTERN = Pattern.compile("^/\\*.*?\\*/");
     private static Pattern CREATE_VIEW_AS_PATTERN = Pattern.compile("(?im)^\\s*(CREATE|ALTER)\\s+?VIEW\\s+?((\\S+?)|(\\[.*\\])|(\\\".*\\\"))\\s+?AS\\s*?", Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
@@ -358,5 +360,51 @@ public class MSSQLDatabase extends AbstractJdbcDatabase {
         } else {
             return caseSensitive.booleanValue();
         }
+    }
+
+    @Override
+    public boolean isReservedWord(String string) {
+        if (reservedWords.contains(string.toUpperCase())) {
+            return true;
+        }
+        return super.isReservedWord(string);
+    }
+    
+    {
+        // list from http://msdn.microsoft.com/en-us/library/ms189822.aspx
+        reservedWords.addAll(Arrays.asList("ADD", "ALL", "ALTER", "AND", "ANY",
+                "AS", "ASC", "AUTHORIZATION", "BACKUP", "BEGIN", "BETWEEN",
+                "BREAK", "BROWSE", "BULK", "BY", "CASCADE", "CASE", "CHECK",
+                "CHECKPOINT", "CLOSE", "CLUSTERED", "COALESCE", "COLLATE",
+                "COLUMN", "COMMIT", "COMPUTE", "CONSTRAINT", "CONTAINS",
+                "CONTAINSTABLE", "CONTINUE", "CONVERT", "CREATE", "CROSS",
+                "CURRENT", "CURRENT_DATE", "CURRENT_TIME", "CURRENT_TIMESTAMP",
+                "CURRENT_USER", "CURSOR", "DATABASE", "DBCC", "DEALLOCATE",
+                "DECLARE", "DEFAULT", "DELETE", "DENY", "DESC", "DISK",
+                "DISTINCT", "DISTRIBUTED", "DOUBLE", "DROP", "DUMP", "ELSE",
+                "END", "ERRLVL", "ESCAPE", "EXCEPT", "EXEC", "EXECUTE",
+                "EXISTS", "EXIT", "EXTERNAL", "FETCH", "FILE", "FILLFACTOR",
+                "FOR", "FOREIGN", "FREETEXT", "FREETEXTTABLE", "FROM", "FULL",
+                "FUNCTION", "GOTO", "GRANT", "GROUP", "HAVING", "HOLDLOCK",
+                "IDENTITY", "IDENTITYCOL", "IDENTITY_INSERT", "IF", "IN",
+                "INDEX", "INNER", "INSERT", "INTERSECT", "INTO", "IS", "JOIN",
+                "KEY", "KILL", "LEFT", "LIKE", "LINENO", "LOAD", "MERGE",
+                "NATIONAL", "NOCHECK", "NONCLUSTERED", "NOT", "NULL", "NULLIF",
+                "OF", "OFF", "OFFSETS", "ON", "OPEN", "OPENDATASOURCE",
+                "OPENQUERY", "OPENROWSET", "OPENXML", "OPTION", "OR", "ORDER",
+                "OUTER", "OVER", "PERCENT", "PIVOT", "PLAN", "PRECISION",
+                "PRIMARY", "PRINT", "PROC", "PROCEDURE", "PUBLIC", "RAISERROR",
+                "READ", "READTEXT", "RECONFIGURE", "REFERENCES", "REPLICATION",
+                "RESTORE", "RESTRICT", "RETURN", "REVERT", "REVOKE", "RIGHT",
+                "ROLLBACK", "ROWCOUNT", "ROWGUIDCOL", "RULE", "SAVE", "SCHEMA",
+                "SECURITYAUDIT", "SELECT", "SEMANTICKEYPHRASETABLE",
+                "SEMANTICSIMILARITYDETAILSTABLE", "SEMANTICSIMILARITYTABLE",
+                "SESSION_USER", "SET", "SETUSER", "SHUTDOWN", "SOME",
+                "STATISTICS", "SYSTEM_USER", "TABLE", "TABLESAMPLE",
+                "TEXTSIZE", "THEN", "TO", "TOP", "TRAN", "TRANSACTION",
+                "TRIGGER", "TRUNCATE", "TRY_CONVERT", "TSEQUAL", "UNION",
+                "UNIQUE", "UNPIVOT", "UPDATE", "UPDATETEXT", "USE", "USER",
+                "VALUES", "VARYING", "VIEW", "WAITFOR", "WHEN", "WHERE",
+                "WHILE", "WITH", "WITHIN GROUP", "WRITETEXT"));
     }
 }
